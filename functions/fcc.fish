2285315a7d6c7f82dd1c26b0a1a20f71071a6d26
erion <robjoy88@gmail.com>
2024-07-30 16:55:56 +0200

    Fix regex for the thousand separator.

--- functions/fcc.fish
+++ functions/fcc.fish
@@ -5,7 +5,7 @@ function fcc -d 'Fish Currency Converter'
         'v/version' 'h/help' -- $argv
     or return 1
 
-    set --local version_fcc '20240730'
+    set --local version_fcc '20240730-2'
 
     if set -q _flag_version
         echo "This is FCC version" $version_fcc
@@ -23,7 +23,7 @@ set -l from (string upper $argv[2])
 set -l to (string upper $argv[3])
 
 # sed with updated regexp (2024-01-29)
-set -l result (curl -sSL "https://www.xe.com/currencyconverter/convert/?Amount=$value&From=$from&To=$to" | sed -n 's/.*<p class="sc-[a-z0-9]*-[0-9] [a-zA-Z]*">\([0-9]*\.[0-9]*\)<span class="faded-digits">.*/\1/p')
+set -l result (curl -sSL "https://www.xe.com/currencyconverter/convert/?Amount=$value&From=$from&To=$to" | sed -n 's/.*<p class="sc-[a-z0-9]*-[0-9] [a-zA-Z]*">\([0-9,]*\.[0-9]*\)<span class="faded-digits">.*/\1/p')
 echo "$value $from = $result $to";
 
     end
